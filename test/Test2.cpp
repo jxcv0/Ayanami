c020b7765d1fa306aa044ed04146984ae6336f18
jxcv0 <jmicoud02@gmail.com>
2021-11-15 21:15:36 +0000

    Fix CMake to tests

--- test/Test2.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-// Demonstrate some basic assertions.
-TEST(secondtest, alsoBasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("test", "two");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
-}
