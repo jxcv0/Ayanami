04fac080592f8b1f709ea6bf6660ef1d66496c36
jxcv0 <jmicoud02@gmail.com>
2021-11-17 20:40:18 +0000

    Initial commit on branch

---
+++ include/Websocket.hpp
@@ -0,0 +1,15 @@
+#ifndef WEBSOCKET_HPP
+#define WEBSOCKET_HPP
+
+#include <thread>
+
+class Websocket: public std::thread {
+    private:
+        /* data */
+    public:
+        Websocket(/* args */);
+};
+
+
+
+#endif
