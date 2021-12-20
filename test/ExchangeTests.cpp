45cc79d6cd64434523ac8437f6228755a1a3ba18
jxcv0 <jmicoud02@gmail.com>
2021-12-20 22:39:27 +0000

    Create strategy dir

--- test/ExchangeTests.cpp
+++ test/ExchangeTests.cpp
@@ -4,9 +4,5 @@
 #include "BybitWSSession.hpp"
 
 TEST(ExchangeTests, connect_to_ws_test) {
-    boost::asio::io_context ioc;
-    ssl::context ctx{ssl::context::tlsv12_client};
     
-    std::make_shared<Ayanami::Exchange::BybitWSSession>(ioc, ctx)->run();
-    ioc.run();
 }
