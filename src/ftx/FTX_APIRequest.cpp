a1538a8ceffc01eab641c609eac89c57e0bf1e21
jxcv <jmicoud02@gmail.com>
2021-12-27 12:03:31 +0000

    Add generate_order_request

--- src/ftx/FTX_APIRequest.cpp
+++ src/ftx/FTX_APIRequest.cpp
@@ -1,5 +1,8 @@
 #include "ftx/FTX_APIRequest.hpp"
 
+#include <cpprest/json.h>
+
+#include <iostream>
 /**
  * @brief Create the JSON for an order reqest to the FTX API
  * 
@@ -14,6 +17,16 @@
  */
 std::string ayanami::ftx::generate_order_request(std::string market, std::string side, double price, std::string type, double size, bool reduce_only, bool post_only) {
     // TODO
+    web::json::keep_object_element_order(true);
+    web::json::value req;
+    req[U("market")] = web::json::value(market);
+    req[U("side")] = web::json::value(side);
+    req[U("price")] = web::json::value(price);
+    req[U("size")] = web::json::value(size);
+    req[U("type")] = web::json::value(type);
+    req[U("reduceOnly")] = web::json::value(reduce_only);
+    req[U("postOnly")] = web::json::value(post_only);
+    return req.serialize().c_str();
 }
 
 /**
--- test/ftx/FTX_APIRequestTests.cpp
+++ test/ftx/FTX_APIRequestTests.cpp
@@ -3,14 +3,17 @@
 #include "ftx/FTX_APIRequest.hpp"
 
 TEST(FTX_APIRequestsTests, generate_order_request_test) {
-    
+    std::string order_req = ayanami::ftx::generate_order_request("BTC-PERP", "buy", 8500, "limit", 1, false, false);
+    std::string example = "{\"market\":\"BTC-PERP\",\"side\":\"buy\",\"price\":8500,\"size\":1,\"type\":\"limit\",\"reduceOnly\":false,\"postOnly\":false}";
+    ASSERT_EQ(example, order_req);
 }
 
 TEST(AuthentificationTests, generate_POST_sign_test) {
     long time = 1588591856950;
     std::string secret = "T4lPid48QtjNxjLUFOcUZghD7CUJ7sTVsfuvQZF2";
+    std::string order_req = ayanami::ftx::generate_order_request("BTC-PERP", "buy", 8500, "Limit", 1, false, true);
 
-    std::string sign = ayanami::ftx::generate_sign(time, "POST", "api/orders", "JSONhere");
+    std::string sign = ayanami::ftx::generate_sign(time, "POST", "api/orders", order_req);
 
     std::string example = "c4fbabaf178658a59d7bbf57678d44c369382f3da29138f04cd46d3d582ba4ba";
     ASSERT_EQ(sign, example);
